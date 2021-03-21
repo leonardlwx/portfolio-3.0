bdf828aa3ab3538492860053292ffa8f8eaaaede
Wei Lim (Leonard) <leonardlwx@gmail.com>
2021-03-21 17:32:23 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="colorlib-page">
+        <Helmet>
+          <title>Leonard Lim</title>
+        </Helmet>
+        <div id="container-wrap">
+          {/*<Sidebar></Sidebar>*/}
+          <div id="colorlib-main">
+              <p> hi</p>
+            {/*<Introduction></Introduction>*/}
+            {/*<About></About>*/}
+            {/*<Timeline></Timeline>*/}
+            {/*<Projects></Projects>*/}
+          </div>
+        </div>
+      </div>
   );
 }
 
